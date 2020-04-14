efb06dcdb8057181f47ab7ad4e68674ed1433936
nanguage <nanguage@yahoo.com>
2020-04-14 14:34:00 +0800

    create tmp dir

--- fisheye/primer_design/extract_tran_seq.py
+++ fisheye/primer_design/extract_tran_seq.py
@@ -1,29 +1,11 @@
 import pandas as pd
 from pyfaidx import Fasta
 
+from fisheye.utils import reverse_complement
+
 gtf_file = 'gencode.vM24.annotation.gtf'
 fa = Fasta('GRCm38.primary_assembly.genome.fa')
 
-def get_base_map():
-    base_map = [b'\0' for i in range(256)]
-    base_map[ ord('A') ] = b'T'
-    base_map[ ord('T') ] = b'A'
-    base_map[ ord('C') ] = b'G'
-    base_map[ ord('G') ] = b'C'
-    base_map[ ord('a') ] = b't'
-    base_map[ ord('t') ] = b'a'
-    base_map[ ord('c') ] = b'g'
-    base_map[ ord('g') ] = b'c'
-    base_map[ ord('N') ] = b'N'
-    base_map[ ord('n') ] = b'n'
-    base_map = bytes(b''.join(base_map))
-    return base_map
-
-BASEMAP = get_base_map()
-def reverse_complement(seq):
-    res = seq[::-1]
-    res = res.translate(BASEMAP)
-    return res
 
 def extract_exons(gtf_file,fa):
     gtf_df = pd.read_csv(gtf_file,sep='\t',header=None)
--- fisheye/primer_design/main.py
+++ fisheye/primer_design/main.py
@@ -10,15 +10,29 @@ import RNA
 import primer3
 import glob
 from fisheye.utils import get_logger
+from fisheye.utils import reverse_complement
 
 
 log = get_logger(__name__)
+
+
+def get_tmp_dir(basename):
+    i = 0
+    dirname = lambda: f"{basename}.{i}"
+    while exists(dirname()):
+        i += 1
+    os.mkdir(dirname())
+    return dirname()
+
+
+TMP_DIR = get_tmp_dir("./primer_tmp")
 
 
 def read_gene(genelist):
     genelist = pd.read_csv(genelist,header = None)
     genelist.columns = ['geneID']
     return genelist
+
 
 def read_gtf(gtf):
     df = pd.read_csv(gtf, sep='\t', header=None)
@@ -27,27 +41,6 @@ def read_gtf(gtf):
     df['length'] = df['end'] - df['start']
     return df
 
-###################################################
-def get_base_map():
-    base_map = [b'\0' for i in range(256)]
-    base_map[ ord('A') ] = b'T'
-    base_map[ ord('T') ] = b'A'
-    base_map[ ord('C') ] = b'G'
-    base_map[ ord('G') ] = b'C'
-    base_map[ ord('a') ] = b't'
-    base_map[ ord('t') ] = b'a'
-    base_map[ ord('c') ] = b'g'
-    base_map[ ord('g') ] = b'c'
-    base_map[ ord('N') ] = b'N'
-    base_map[ ord('n') ] = b'n'
-    base_map = bytes(b''.join(base_map))
-    return base_map
-
-BASEMAP = get_base_map()
-def reverse_complement(seq):
-    res = seq[::-1]
-    res = res.translate(BASEMAP)
-    return res
 
 def sequence_pickup(df_gtf, fa, genelist, min_length=40 ):
     seq_lst = {}
@@ -67,7 +60,7 @@ def sequence_pickup(df_gtf, fa, genelist, min_length=40 ):
         seq_lst[name] = seq
     return seq_lst
 
-#####################################################
+
 def self_match(probe, min_match = 4):
     length = len(probe)
     probe_re = reverse_complement(probe)
@@ -118,11 +111,23 @@ def cal_weight(df):
     w.columns = ['point1', 'point2', 'tm_region', 'RNAfold_score']
     return w
 
+
+def write_fastq(gene, seqs):
+    with open(f'{TMP_DIR}/{gene}.fa', 'w') as f:
+        for i, seq in enumerate(seqs):
+            f.write(f"@{gene}_{i}\n")
+            f.write(seq+"\n")
+            f.write("+\n")
+            f.write("~"*len(seq)+"\n")
+
+
 def primer_design(name, seq, min_length=40):
     df_lst = []
     seq_len = len(seq)
+    sub_seqs = []
     for i in range(0,len(seq)-min_length+1):
         tem = seq[i:min_length+i]
+        sub_seqs.append(tmp)
         fold_score = round(RNA.fold_compound(tem).mfe()[1],2)
         tem1 = tem[0:13]
         tem2 = tem[13:26]
@@ -139,9 +144,12 @@ def primer_design(name, seq, min_length=40):
         amp_probe = tem3_re+tem4+"ACTGCAGGCTCCA"
         match_pairs_pad = self_match(pad_probe)
         match_pairs_amp = self_match(amp_probe)
-        with open(f'{name}.fa', 'a+') as f:
-            f.write('>' + name + '\t' + str(i) + '\n' + tem + '\n')
         df_lst.append([match_pairs_pad, match_pairs_amp, region, tm1, tm2, tm3, fold_score, pad_probe, amp_probe])
+    
+    write_fastq(name, sub_seqs)
+    # align
+    # processing sam -> number of alns 
+
     df = pd.DataFrame(df_lst)
     df.columns = ['point1', 'point2', 'tm_region', 'tm1', 'tm2', 'tm3', 'RNAfold_score', 'primer1', 'primer2']
 
@@ -155,19 +163,6 @@ def primer_design(name, seq, min_length=40):
     df = df.reset_index(drop=True)
     return df
 
-def write_fq():
-    for i in glob.glob('*.fa'):
-        with open(i, 'r') as f1:
-            N = 0
-            with open("%s.fq" % i[:-3], 'w') as f2:
-                for line in f1:
-                    N += 1
-                    if N % 2 == 1:
-                        f2.write('@' + line[1:])
-                    else:
-                        length = len(line[5:-5])
-                        f2.write(line + '+' + '\n' + '>' * 5 + 'G' * length + 'B' * 4 + '\n')
-
 def main(genelist, gtf, fasta, output_dir="primers"):
     """
     input: genelist gtf fasta
@@ -178,6 +173,7 @@ def main(genelist, gtf, fasta, output_dir="primers"):
     genelist = read_gene(genelist)
     df_gtf = read_gtf(gtf)
     log.info("pickup seqences..")
+    log.info(f"Create tmp dir: {TMP_DIR}, fastq, sam... files will save to it")
     seq_lst = sequence_pickup(df_gtf, fa, genelist, min_length=40)
     # TODO: calculate specificity:
     # 1. save all gene's all transcripts to fasta
@@ -191,7 +187,6 @@ def main(genelist, gtf, fasta, output_dir="primers"):
     for name, seq in seq_lst.items():
         log.info("Designing primer for gene " + name + ":")
         res_df = primer_design(name,seq)
-        write_fq()
         best.append(res_df.iloc[0, :])
         out_path = join(output_dir, f"{name}.csv")
         log.info("Save results to: " + out_path)
